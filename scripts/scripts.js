4961bcc0198c2a6d296e68c435e56c38c359c2ef
vacarneiro <100733452+vacarneiro@users.noreply.github.com>
2022-06-14 20:23:27 -0300

    google verify

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,49 +1,45 @@
-// Includes html snippets
+// ========== Includes html snippets
 
 function includeHTML() {
-    var z, i, elmnt, file, xhttp;
-    /* Loop through a collection of all HTML elements: */
-    z = document.getElementsByTagName("*");
-    for (i = 0; i < z.length; i++) {
-      elmnt = z[i];
-      /*search for elements with a certain atrribute:*/
-      file = elmnt.getAttribute("w3-include-html");
-      if (file) {
-        /* Make an HTTP request using the attribute value as the file name: */
-        xhttp = new XMLHttpRequest();
-        xhttp.onreadystatechange = function() {
-          if (this.readyState == 4) {
-            if (this.status == 200) {elmnt.innerHTML = this.responseText;}
-            if (this.status == 404) {elmnt.innerHTML = "Page not found.";}
-            /* Remove the attribute, and call this function once more: */
-            elmnt.removeAttribute("w3-include-html");
-            includeHTML();
-          }
+  var z, i, elmnt, file, xhttp;
+  /* Loop through a collection of all HTML elements: */
+  z = document.getElementsByTagName("*");
+  for (i = 0; i < z.length; i++) {
+    elmnt = z[i];
+    /*search for elements with a certain atrribute:*/
+    file = elmnt.getAttribute("w3-include-html");
+    if (file) {
+      /* Make an HTTP request using the attribute value as the file name: */
+      xhttp = new XMLHttpRequest();
+      xhttp.onreadystatechange = function () {
+        if (this.readyState == 4) {
+          if (this.status == 200) { elmnt.innerHTML = this.responseText; }
+          if (this.status == 404) { elmnt.innerHTML = "Page not found."; }
+          /* Remove the attribute, and call this function once more: */
+          elmnt.removeAttribute("w3-include-html");
+          includeHTML();
         }
-        xhttp.open("GET", file, true);
-        xhttp.send();
-        /* Exit the function: */
-        return;
       }
+      xhttp.open("GET", file, true);
+      xhttp.send();
+      /* Exit the function: */
+      return;
     }
   }
+}
 
-// Tried to show confirm box when Mestre link is clicked
+// ========== Creates collapsibles in historico.html
+var coll = document.getElementsByClassName("collapsible");
+var i;
 
-  var elems = document.getElementsByClassName('confirmation');
-  var confirmIt = function (e) {
-      if (!confirm('Você tem certeza?')) e.preventDefault();
-  };
-  for (var i = 0, l = elems.length; i < l; i++) {
-      elems[i].addEventListener('click', confirmIt, false);
-  }
-
-  // Toggle visibility of class "section"
-  function toggleVisibility() {
-    var x = document.getElementByClassName("session");
-    if (x.style.display === "none") {
-      x.style.display = "block";
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
     } else {
-      x.style.display = "none";
+      content.style.display = "block";
     }
-  }
+  });
+}
