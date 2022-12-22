5e3b819051137f476c9f20dd850b6ce892ff70c8
vacarneiro <100733452+vacarneiro@users.noreply.github.com>
2022-12-22 09:26:43 -0300

    JSON working in vila-monforte.html

--- scripts/locais-json.js
+++ scripts/locais-json.js
@@ -1,23 +1,10 @@
 // ========== Populates pages-locais with info from JSON ==========
 
-// $.getJSON("https://vacarneiro.github.io/pages-locais/locais.json", function(data){
-//     console.log(data);
-// })
-
-// let obj = JSON.parse(text);
-// let json_localName = obj.name;
-// let localName = document.getElementById("local-name");
-//     localName.textContent = 'Muda diabo';
-
-
-
-// ========== MDN tutorial ==========
-
+// Pulls JSON file from Github and assigns it to variable "localInfo"
 async function populate() {
 
     const requestURL = 'https://vacarneiro.github.io/pages-locais/locais.json';
     const request = new Request(requestURL);
-
     const response = await fetch(request);
     const localInfo = await response.json();
 
@@ -25,17 +12,19 @@ async function populate() {
 
 }
 
+populate();
 
+// Gets id from html elements and changes content according to JSON
 function populateLocal(obj) {
 
     const localName = document.getElementById('local-name');
-    localName.textContent = obj.name[1];
-   
+    const localSuserano = document.getElementById('local-suserano');
+    const localMeirinho = document.getElementById('local-meirinho');
+
+    localName.textContent = obj.locals[1].name;
+    localSuserano.textContent = obj.locals[1].suserano;
+    localMeirinho.textContent = obj.locals[1].meirinho;
 
 }
 
 populateLocal();
-
-
-// let testando = document.getElementById('local-name');
-// testando.textContent = 'Muda diabo';
